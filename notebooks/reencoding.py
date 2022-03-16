fc35cd2004c3daf1f6f41c541e4368e3015f2436
Mark Mazumder <mark@markmaz.com>
2022-03-15 23:54:54 -0400

    reencoding script

---
+++ notebooks/reencoding.py
@@ -0,0 +1,91 @@
+# %%
+import datetime
+import multiprocessing
+import os
+import json
+from pathlib import Path
+import numpy as np
+import tqdm
+import sox
+from dataclasses import dataclass
+import pickle
+
+# %%
+with open("metadata.json", "r") as fh:
+    meta = json.load(fh)
+
+@dataclass
+class Task:
+    clip_dir: Path
+    sample_opus: str
+    word: str
+    basedir: Path
+
+target_dir = Path("/mnt/disks/std4/16khz_wav")
+
+def convert_16k_wav(task:Task):
+    wav = Path(task.sample_opus).stem + ".wav"
+    original_fp = str(task.basedir / task.word / wav)
+    dest = str(task.clip_dir / wav)
+    transformer = sox.Transformer()
+    transformer.convert(samplerate=16000)
+    transformer.build(original_fp, dest)
+    return task
+
+#fmt: off
+mswc_languages = ['sl', 'br', 'ro', 'rm-sursilv', 'el', 'mt', 'id', 'sah', 'fy-NL', 'cv', 'sk', 'ia', 'lv', 'vi', 'ar', 'as', 'or', 'gn', 'sv-SE', 'dv', 'ta', 'rm-vallader', 'ka', 'zh-CN', 'cnh', 'ha', 'ga-IE', 'ky', 'mn', 'tr', 'lt', 'uk', 'et', 'cs', 'tt', 'pt', 'nl', 'cy', 'ru', 'eo', 'fa', 'eu', 'pl', 'rw', 'ca', 'it', 'de', 'en', 'es', 'fr']
+assert len(mswc_languages) == 50
+std3_ignore = ['ab', 'gl']
+std3_langs = ['br', 'cv', 'gn', 'de', 'el', 'en', 'fa', 'ca', 'ha', 'lt', 'pt', 'ru', 'rw', 'sk', 'ta', 'tr', 'tt', 'uk', 'zh-CN']
+for lang in std3_langs:
+    assert lang in mswc_languages, f"{lang} missing"
+#fmt:on
+
+verify = False
+if verify:
+    for lang_isocode in std3_langs:
+        print(f"verifying all clips in MSWC metadata are present on disk for {lang_isocode}")
+        basedir = Path(f"/mnt/disks/std3/data/generated/common_voice/frequent_words/{lang_isocode}/clips")
+        uhoh = 0
+        considered = 0
+        for keyword, samples in tqdm.tqdm(meta[lang_isocode]["filenames"].items()):
+            for sample in samples:
+                wav = Path(sample).stem + ".wav"
+                fp = basedir / keyword / wav
+                if not fp.exists():
+                    uhoh += 1
+                considered += 1
+        print(lang_isocode, considered, uhoh)
+        assert uhoh == 0, "missing source wavfiles"
+
+
+for lang_isocode in std3_langs:
+    if lang_isocode in ["en", "de", "sk", "br"]:
+        continue # already done
+    basedir = Path(f"/mnt/disks/std3/data/generated/common_voice/frequent_words/{lang_isocode}/clips")
+    full_name = meta[lang_isocode]["language"]
+    print(f"\n\n starting {lang_isocode} -------- {full_name}")
+
+    tasks = []
+    keywords = meta[lang_isocode]["wordcounts"].keys()
+    for word in keywords:
+        clip_dir = target_dir / lang_isocode / "clips" / word
+        os.makedirs(clip_dir, exist_ok=True)
+        for sample_opus in meta[lang_isocode]["filenames"][word]:
+            tasks.append(Task(clip_dir=clip_dir, sample_opus=sample_opus, word=word, basedir=basedir))
+
+    total = len(tasks)
+    print("tasks:", total)
+    five_pct = int(len(tasks) * 0.05)
+
+    done = 0
+    with multiprocessing.Pool() as p:
+        for i in p.imap_unordered(convert_16k_wav, tasks, chunksize=200):
+            done += 1
+            if done % five_pct == 0:
+                print(f"{lang_isocode} {done=} {total=}", done / total)
+    now = datetime.datetime.now()
+    with open(f"logs/{lang_isocode}__{now}.log", 'w') as fh:
+        fh.write(f"{done}\n")
+    print(f"{lang_isocode} all done", done, len(tasks), "pct:", done / len(tasks), now)
+# %%
