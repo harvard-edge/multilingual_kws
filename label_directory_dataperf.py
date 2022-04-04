195f031eb7ba89bff30e1aaf9506393485f143b3
Colby Banbury <cbanbury@g.harvard.edu>
2022-04-04 17:55:26 -0400

    fix summary and add the ability to save and restart mid way through a word with 's'

--- label_directory_dataperf.py
+++ label_directory_dataperf.py
@@ -29,6 +29,18 @@ class _GetchUnix:
 
 
 def label(args):
+
+    already_listened = []
+    if os.path.isfile(args.out_csv):
+        previous_file = Path(args.out_csv)
+        print("datadir:", previous_file)
+        with open(previous_file, "r") as fh:
+            reader = csv.reader(fh)  #file, good?
+            for row in reader:
+                already_listened.append(row[0])
+
+    already_listened = set(already_listened)
+
     datadir = Path(args.datadir)
     print("datadir:", datadir)
 
@@ -44,6 +56,8 @@ def label(args):
             if row[1] == args.word:
                 opus = row[0]  # aachen/common_voice_en_18833718.opus
                 wav = opus.replace("opus", "wav")
+                if wav in already_listened:
+                    continue
                 wav_list.append(wav)
 
     test_splits = splitdir / "en_test.csv"
@@ -53,6 +67,8 @@ def label(args):
             if row[1] == args.word:
                 opus = row[0]
                 wav = opus.replace("opus", "wav")
+                if wav in already_listened:
+                    continue
                 wav_list.append(wav)
     
     print("len(wav_list):", len(wav_list))
@@ -65,6 +81,7 @@ def label(args):
     for ix, clip in enumerate(wav_list):
         print(f"\n:::::: CLIP # {ix} :::", clip)
         fpath = str(datadir / clip)
+        stop_and_save = False
         while True:
             print("-")
             wav = pydub.AudioSegment.from_wav(fpath)
@@ -78,6 +95,9 @@ def label(args):
             elif choice == "b":
                 usable = False
                 break
+            elif choice == "s":
+                stop_and_save = True
+                break 
             elif choice == "q":
                 sys.exit()
 
@@ -85,16 +105,18 @@ def label(args):
         row = [clip, result]
         print(row)
         results.append(row)
+        if stop_and_save:
+            break #allows stopping and saving part of the way through a word
 
     if not args.dryrun:
-        out_csv = datadir / f"{args.word}_manual_listening_results.csv"
-        with open(out_csv, "w") as fh:
+        out_csv = args.out_csv
+        with open(out_csv, "a") as fh: #append to exisiting file
             writer = csv.writer(fh)
             writer.writerows(results)
     # summary
     print("\n\n\n\n:::::::::: SUMMARY  ")
-    print("num good:", len([g for g in results if g[2] == "good"]))
-    print("num bad:", len([g for g in results if g[2] == "bad"]))
+    print("num good:", len([g for g in results if g[1] == "good"]))
+    print("num bad:", len([g for g in results if g[1] == "bad"]))
     if not args.dryrun:
         print(f">>>>>> results written to {out_csv}")
 
@@ -104,6 +126,7 @@ if __name__ == "__main__":
     parser.add_argument("datadir", help="directory of wavs")
     parser.add_argument("splitdir", help="directory of split csvs")
     parser.add_argument("word", help="word to analyze")
+    parser.add_argument("out_csv", help="output filepath. location of checkpoint if previously saved")
     parser.add_argument('--dryrun', action="store_true", help="do not write to output csv file")
     group = parser.add_mutually_exclusive_group()
     label(parser.parse_args())
